2cfa50c8f47a5b540e9597aaabbe0122744d785a
amandamayfield <amandamayfield.designs@gmail.com>
2025-09-02 17:39:34 -0500

    feat: added nav button styling

--- src/components/NavButton.vue
+++ src/components/NavButton.vue
@@ -12,7 +12,7 @@ watchEffect(() => {
 </script>
 
 <template>
-  <div class="nav-btn">
+  <div class="nav-btn" aria-label="Navigation Button">
     <div v-if="!isHome">
       <RouterLink class="nav-btn__link" :to="{name: 'home'}">Home</RouterLink>
     </div>
@@ -23,8 +23,37 @@ watchEffect(() => {
 </template>
 
 <style lang="scss" scoped>
+@use './../assets/base.scss';
+
 .nav-btn {
-  padding: 16px;
-  background-color: darkgreen;
+  position: absolute;
+  bottom: -40px;
+  left: 50%;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  transform: translateX(-50%);
+  background-color: var(--burnt-brown);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
+}
+
+@media (max-width: 1280px) {
+  .nav-btn {
+    bottom: -30px;
+    width: 60px;
+    height: 60px;
+  }
+}
+
+@media (max-width: 600px) {
+  .nav-btn {
+    bottom: -25px;
+    width: 50px;
+    height: 50px;
+  }
 }
 </style>
